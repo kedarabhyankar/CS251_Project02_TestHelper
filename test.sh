c01528b3a183d2a34af4b23795a80f494d1ac8ac
Kedar Abhyankar <krabhyankar@gmail.com>
2020-02-24 09:45:42 -0500

    Fixed Spelling Error + changed from 'git diff' to 'diff' from diffutils 2.8.1 circa 2002'

--- test.sh
+++ test.sh
@@ -3,31 +3,31 @@ javac RedBlackBST.java -Xlint:unchecked
 if [ "$1" = "insert" ]; then
   echo "Running Insert!"
   java RedBlackBST ../insert.txt > out.txt
-  git diff -w ../insertExpected.txt out.txt
+  diff -w ../insertExpected.txt out.txt
 fi
 
 if [ "$1" = "delete" ]; then
   echo "Running Delete!"
   java RedBlackBST ../delete.txt > out.txt
-  git diff -w ../deleteExpectred.txt out.txt
+  diff -w ../deleteExpected.txt out.txt
 fi
 
 if [ "$1" = "robustness" ]; then
   echo "Running Robustness!"
   java RedBlackBST ../test.txt > out.txt
-  git diff -w ../testExpected.txt out.txt
+  diff -w ../testExpected.txt out.txt
 fi
 
 if [ "$1" = "search" ]; then
   echo "Running Search!"
   java RedBlackBST ../search.txt > out.txt
-  git diff -w ../searchExpected.txt out.txt
+  diff -w ../searchExpected.txt out.txt
 fi
 
 if [ "$1" = "rank" ]; then
   echo "Running Rank!"
   java RedBlackBST ../rank.txt > out.txt
-  git diff -w ../rankExpected.txt out.txt
+  diff -w ../rankExpected.txt out.txt
 fi
 
 if [ "$2" == "--w" ]; then
